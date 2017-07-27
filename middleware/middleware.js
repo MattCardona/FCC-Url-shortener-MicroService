774035f81ddd69e07cf6562b173276c58c1abe47
Matt <boblikesbbq@gmail.com>
2017-07-27 10:00:23 -0700

    Initial commit

---
+++ middleware/middleware.js
@@ -0,0 +1,36 @@
+const validator = require('validator');
+
+var obj = {}
+
+var midd = (req, res, next) => {
+  var url = req.params[0];
+  var fullUrl = req.protocall + req.get('host') + req.url;
+  var redirectUrl = req.protocall + req.get('host') + '/new/';
+
+  if(obj[url] === undefined){
+    if(!validator.isURL(url)){
+      res.status(400).send("Error the URL is invalid, please check for errors.");
+      req.obj = {error: "Error the URL is invalid, please check for errors."};
+      next();
+    }else{
+      obj[url]= {
+        host: req.protocall + req.get('host'),
+        ogUrl: url,
+        full: fullUrl,
+        reUrl: Math.floor(Math.random() * 20) + 1
+         }
+      obj[obj[url].reUrl]={
+        ogUrl: url
+       }
+       req.obj = {url, fullUrl, redirectUrl: redirectUrl + obj[url].reUrl};
+       next();
+     }
+  }else if(obj[url] !== undefined){
+    var copy = obj[url].ogUrl;
+    res.redirect(301, obj[url].ogUrl);
+    delete obj[copy];
+    delete obj[url];
+  }
+};
+
+module.exports = { midd }
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const {midd} = require('./middleware/middleware.js');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.set('view engine', 'hbs');
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index.hbs');
+});
+
+app.get('/new/*', midd, (req, res) => {
+  res.json(req.obj);
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port : ${port}`);
+});
